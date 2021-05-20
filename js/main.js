28cc35376fb66e2ca9a6f6eb68598c4122c5ebd1
Randy <randy_maclean@hotmail.ca>
2021-05-20 15:18:00 -0700

    Responsive jQuery

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
-    $(function() {
-        if(jQuery(window).width() < 768) {
-            $('.headline-form-card').css({opacity: 0, top: "750px"});
-            $(document).ready(function() {
-                $('.headline-form-card').animate({opacity: 1, top: "235px"}, 1000);  
-            })
-         } else if (jQuery(window).width() < 1024 && jQuery(window).width() > 768) {
-            $('.headline-form-card').css({opacity: 0, top: "750px"});
-            $(document).ready(function() {
-                $('.headline-form-card').animate({opacity: 1, top: "500px"}, 1000);
-            });
-         }   
-        })
+    screenWidth = $( window ).width();
+    $('.headline-form-card').css({opacity: 0, top: "750px"});
+    $(document).ready(function() {
+        if (screenWidth < 481) {
+            $('.headline-form-card').animate({opacity: 1, top: "200px"}, 1000);  
+        } else if (screenWidth > 481 && screenWidth < 769) {
+            $('.headline-form-card').animate({opacity: 1, top: "235px"}, 1000); 
+        } else  if (screenWidth > 769 && screenWidth < 1025) {
+            $('.headline-form-card').animate({opacity: 1, top: "255px"}, 1000);  
+        } else {
+            $('.headline-form-card').animate({opacity: 1, top: "325px"}, 1000); 
+        }
     });
+});
+
+
