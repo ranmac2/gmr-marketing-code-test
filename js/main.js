e7d7fc1a6930a241cf0738df6f5bdb11424d1cbc
Randy <randy_maclean@hotmail.ca>
2021-05-20 15:50:13 -0700

    Functionality to secondary cta

--- js/main.js
+++ js/main.js
@@ -5,13 +5,17 @@ $(document).ready(function(){
         if (screenWidth < 481) {
             $('.headline-form-card').animate({opacity: 1, top: "200px"}, 1000);  
         } else if (screenWidth > 481 && screenWidth < 769) {
-            $('.headline-form-card').animate({opacity: 1, top: "235px"}, 1000); 
+            $('.headline-form-card').animate({opacity: 1, top: "225px"}, 1000); 
         } else  if (screenWidth > 769 && screenWidth < 1025) {
-            $('.headline-form-card').animate({opacity: 1, top: "255px"}, 1000);  
+            $('.headline-form-card').animate({opacity: 1, top: "235px"}, 1000);  
         } else {
-            $('.headline-form-card').animate({opacity: 1, top: "325px"}, 1000); 
+            $('.headline-form-card').animate({opacity: 1, top: "330px"}, 1000); 
         }
     });
-});
 
+    $('.secondary-cta-text').css({opacity: 0, "margin-right": 300});
+    $(document).scroll(function() {
+        $('.secondary-cta-text').animate({opacity: 1, "margin-right": 35}, 1500);  
+    });
+});
 
